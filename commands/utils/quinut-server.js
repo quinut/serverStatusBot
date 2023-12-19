a35ec33bdf88ce7f3d0dc7259892ffda71ef4eb2
quinut <66158094+quinut@users.noreply.github.com>
2023-12-19 22:00:53 +0900

    good

--- commands/utils/quinut-server.js
+++ commands/utils/quinut-server.js
@@ -1,26 +1,55 @@
 const { SlashCommandBuilder, EmbedBuilder, Attachment } = require('discord.js');
 const wait = require('node:timers/promises').setTimeout;
-// const convertBase64ToURL = require('../../base64toURL.js');
-//https://discordjs.guide/slash-commands/response-methods.html#deferred-responses
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('서버상태')
     .setDescription('퀴넛서버를 확인해요!')
-    .addStringOption(option =>
-			option
-				.setName('ip')
-				.setDescription('서버의 주소를 쓰세요.')),
-    // .addStringOption(option =>
-    //   option.setName('IP')
-    //     .setDescription('상태를 확인할 서버의 IP주소')),
+    
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('퀴넛')
+        .setDescription('퀴넛서버를 확인합니다.'))
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('태롤')
+        .setDescription('태롤서버를 확인합니다.'))
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('도메인')
+        .setDescription('서버 도메인을 직접 입력합니다.')
+        .addStringOption(option =>
+          option
+            .setName('domain')
+            .setDescription('서버 도메인')
+            .setRequired(true))),
+
+
   async execute(interaction) {
     await interaction.deferReply();
     await wait(4000);
-    const serverIP = interaction.options.getString('ip') ?? 'quinut.kro.kr';
+    let serverIP;
+    const subCommand = interaction.options.getSubcommand()
+
+    switch (subCommand) {
+      case '퀴넛':
+        serverIP = 'quinut.kro.kr'
+        break;
+      case '태롤':
+        serverIP = 'tjbbak.kro.kr'
+        break;
+      case '도메인':
+        serverIP = interaction.options.getString('domain')
+        break;
+      default:
+        serverIP = 'quinut.kro.kr'
+
+
+    }
     await fetch(`https://api.mcsrvstat.us/3/${serverIP}`)
       .then(response => response.json())
       .then(data => {
-        console.log(data);
+
 
         if (data.online) {
           const onlinePlayer = data.players.online
@@ -30,15 +59,13 @@ module.exports = {
           const serverVersion = (data.debug.ping) ? protocol : version
 
 
-          const attachment = new Attachment('https://i.imgur.com/AfFp7pu.png', 'test.gif');
-
           const statusEmbed = new EmbedBuilder()
             .setColor("#57F287")
             .setTitle(serverIP)
             .setURL(`https://mcsrvstat.us/server/${serverIP}`)
             .setAuthor({ name: "온라인", iconURL: "https://github.com/quinut/serverStatusBot/blob/main/image/%2357F287.png?raw=true", url: 'https://discord.gg' })
             .setDescription('서버가 온라인입니다.')
-            .setThumbnail('attachment://test.gif')
+            // .setThumbnail('')
             .addFields(
               { name: '플레이어', value: `${onlinePlayer} / ${maxPlayer}` },
               { name: '\u200B', value: '\u200B' },
@@ -59,9 +86,7 @@ module.exports = {
       }
 
       
-        
-      
-		  
+
     })
     .catch(error => {
       interaction.editReply({ content: '커맨드 실행에 실패했습니다!', ephemeral: true });

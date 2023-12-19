0991cd9efa824e3cefd64d7d8d2bba0a2a959658
quinut <66158094+quinut@users.noreply.github.com>
2023-12-19 18:04:28 +0900

    added color image

--- apitest.js
+++ apitest.js
@@ -1,10 +1,21 @@
+let itIs;
+
+
 fetch('https://api.mcsrvstat.us/3/quinut.kro.kr')
-    .then(response => response.json())
-    .then(data => {
-      const status = (data.online ? "Online" : "Offline")
-      console.log(status);
-    })
-    .catch(error => {
-      interaction.reply({ content: '커맨드 실행에 실패했습니다!', ephemeral: true });
-      console.error(error);
-    });
+  .then(response => response.json())
+  .then(data => {
+    constIt(data);
+  })
+  .catch(error => {
+    interaction.reply({ content: '커맨드 실행에 실패했습니다!', ephemeral: true });
+    console.error(error);
+});
+
+
+
+
+function constIt(a) {
+  itIs = a
+}
+
+console.log(itIs);
